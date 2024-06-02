9c61693ab3ee83d43555c5eac38d36e53ee2a784
A.SANJAY <123942701+sanjay302002@users.noreply.github.com>
2024-06-02 14:02:46 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,26 +82,9 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="#"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
+            
 
-            <Nav.Item>
-              <Nav.Link
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
+            
             <Nav.Item className="fork-btn">
               <Button
                 href="#"
