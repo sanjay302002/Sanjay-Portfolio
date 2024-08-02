ab014eabd6e73908666a236527e3ba902464a7df
A.SANJAY <123942701+sanjay302002@users.noreply.github.com>
2024-08-02 20:11:15 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,12 +14,13 @@ function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
-        <Col md="4" className="footer-copywright">
+        
+        {/* <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} </h3>
+        </Col> */}
+        {/* <Col md="4" className="footer-copywright">
           <h3>Designed and Developed by Ritheesh MK</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SV</h3>
-        </Col>
+        </Col> */}
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
