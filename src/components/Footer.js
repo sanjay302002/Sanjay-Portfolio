e5c19f76bdef4c0514a94a1808a8efd6bfad38f5
A.SANJAY <123942701+sanjay302002@users.noreply.github.com>
2024-08-08 21:15:24 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,31 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitterSquare  } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        
+        {/* <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} </h3>
+        </Col> */}
+        {/* <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Ritheesh MK</h3>
+        </Col> */}
+        
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
 
