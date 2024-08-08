f06e520ea281330c2582919dcd22ea70ee79f57b
A.SANJAY <123942701+sanjay302002@users.noreply.github.com>
2024-08-08 21:16:49 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/Design_Inspiration___76-removebg-preview.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
