dc304614a01b27a5e99f212e617a2e8db14ca714
A.SANJAY <123942701+sanjay302002@users.noreply.github.com>
2024-08-16 12:58:16 +0530

    About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,18 +21,18 @@ function About() {
               paddingBottom: "50px",
             }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            {/* <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
               Know Who <strong className="purple">I'M</strong>
-            </h1>
+            </h1> */}
             <Aboutcard />
           </Col>
-          <Col
+          {/* <Col
             md={5}
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
             <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
+          </Col> */}
         </Row>
         <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
